6ec3a459d32d9da94a368577272d2daf5a542c99
cyril <cyril@debian>
2016-11-07 20:41:38 +0300

    Added config files

---
+++ company-settings.el
@@ -0,0 +1,20 @@
+;; (global-company-mode)
+
+;; Enable company-mode only for local filenames with prog-mode
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (let ((filename (buffer-file-name)))
+              (when filename
+                (if (file-remote-p filename)
+                    nil
+                  (progn (company-mode t)
+                         (projectile-mode t)))))))
+
+;; Enable lines numbering for prog-mode
+(add-hook 'prog-mode-hook 'linum-mode)
+
+;; (global-set-key (kbd "M-\\") 'company-complete-common)
+(global-set-key (kbd "M-\\") 'helm-company)
+(eval-after-load 'company-mode '(define-key company-mode-map (kbd "M-\\") 'helm-company))
+
+(provide 'company-settings)
