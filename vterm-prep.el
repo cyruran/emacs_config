078caca39a41517b41025efe1c478c596d249975
Kirill Moiseevskikh <cyruran@mail.ru>
2022-12-03 20:14:16 +0300

    Various vterm-prep fixes

--- vterm-prep.el
+++ vterm-prep.el
@@ -1,29 +1,33 @@
 (require 'company)
 
 (define-minor-mode vterm-prep-mode ""
-  :keymap `((,(kbd "C-c C-c") . vterm-prep-send)
-            (,(kbd "C-c C-;") . ,(lambda ()
+  :keymap (list
+           (cons (kbd "C-c C-a") #'vterm-prep-send)
+           (cons (kbd "C-c C-;") (lambda ()
                                    (interactive)
                                    (comment-region (point-min) (point-max))
                                    (goto-char (point-max))
                                    (newline)))
-            (,(kbd "<header-line> <mouse-3>") . ,(lambda ()
-                                                   (interactive)
-                                                   (let ((window (window-buffer
-                                                                  (window-at (cadr (mouse-position))
-                                                                             (cddr (mouse-position))
-                                                                             (car (mouse-position))))))
-                                                     (with-current-buffer window
-                                                       (pop-to-buffer vterm-prep-assosiated-buffer nil t)))))
-            (,(kbd "M-\\") . helm-company)
-            (,(kbd "C-c C-u") . erase-buffer)
-            (,(kbd "C-c C-k") . kill-buffer)
-            (,(kbd "C-c C-v") . (lambda (start end)
-                                  (interactive "r")
-                                  (vterm-prep-send
-                                   (if (use-region-p)
-                                       (buffer-substring start end)
-                                     (thing-at-point 'line t))))))
+            (cons (kbd "<header-line> <mouse-3>")
+                  (lambda ()
+                    (interactive)
+                    (let ((window (window-buffer
+                                   (window-at (cadr (mouse-position))
+                                              (cddr (mouse-position))
+                                              (car (mouse-position))))))
+                      (with-current-buffer window
+                        (pop-to-buffer vterm-prep-assosiated-buffer nil t)))))
+            (cons (kbd "M-\\") #'helm-company)
+            (cons (kbd "C-c C-u") #'erase-buffer)
+            (cons (kbd "C-c C-k") (lambda ()
+                                    (interactive)
+                                    (kill-buffer (current-buffer))))
+            (cons (kbd "C-c C-c") (lambda (start end)
+                                    (interactive "r")
+                                    (vterm-prep-send
+                                     (if (use-region-p)
+                                         (buffer-substring start end)
+                                       (thing-at-point 'line t))))))
   :lighter "[prep]")
 
 
@@ -35,16 +39,21 @@
      (setq header-line-format (propertize ">> DISCONNECTED" 'face '(:foreground "red")))
      nil)))
 
+(defun vterm-prep--get-cmdline ()
+  (buffer-string))
+
 (defun vterm-prep-send (&optional string)
   (interactive)
   (when (vterm-prep-check-buffer-or-let-know)
     (let ((content (or string
-                       (buffer-substring (point-min) (point-max)))))
-      (with-current-buffer vterm-prep-assosiated-buffer
-        (vterm-insert content)
-        (vterm-send-return))
-      (if (vterm-prep-check-buffer-or-let-know)
-          (pop-to-buffer vterm-prep-assosiated-buffer nil t)))))
+                       (vterm-prep--get-cmdline))))
+      (if (and (stringp content)
+               (not (string= content "")))
+          (with-current-buffer vterm-prep-assosiated-buffer
+            (vterm-insert content)
+            (vterm-send-return))
+        (if (vterm-prep-check-buffer-or-let-know)
+            (pop-to-buffer vterm-prep-assosiated-buffer nil t))))))
 
 (defun vterm-prep-compl-get-content ()
   (buffer-substring-no-properties (line-beginning-position) (point)))
@@ -79,6 +88,16 @@
     (candidates (vterm-prep-compl-gather-completions arg))
     (meta (format "This value is named %s" arg))))
 
+(defun vterm-prep--init ()
+  (sh-mode)
+  (vterm-prep-mode)
+  (company-mode t)
+  (make-local-variable 'vterm-prep-assosiated-buffer)
+  (setq vterm-prep-assosiated-buffer vterm-buffer)
+  (setq header-line-format (propertize (format ">> %s" vterm-buffer) 'face '(:slant italic)))
+  (setq-local company-backends (cons #'vterm-prep-backend company-backends))
+  (setq-local company-idle-delay nil))
+
 (defun vterm-prep (arg)
   (interactive "P")
   (when (eq major-mode 'vterm-mode)
@@ -89,27 +108,41 @@
            (new-buff (lambda ()
                        (generate-new-buffer vterm-prep-buffer-name))))
       (message "%s" arg)
-      (switch-to-buffer-other-window (if arg
-                                         (funcall new-buff)
-                                       (aif (get-buffer vterm-prep-buffer-name)
-                                            it
-                                            (funcall new-buff))))
-      (sh-mode)
-      (vterm-prep-mode)
-      (company-mode t)
-      (make-local-variable 'vterm-prep-assosiated-buffer)
-      (setq vterm-prep-assosiated-buffer vterm-buffer)
-      (setq header-line-format (propertize (format ">> %s" vterm-buffer) 'face '(:slant italic)))
-      (setq-local company-backends (cons #'vterm-prep-backend company-backends))
-      (setq-local company-idle-delay nil))))
+
+      (let* ((buffer (get-buffer vterm-prep-buffer-name))
+             (new (or arg (not buffer))))
+        (switch-to-buffer-other-window (if new
+                                           (generate-new-buffer vterm-prep-buffer-name)
+                                         buffer))
+        (if new
+            (vterm-prep--init))
+        (if (boundp 'vterm-prep--new-command-value)
+            (insert vterm-prep--new-command-value))))))
 
 (defun vterm-prep-send-to-prep (start end)
   (interactive "r")
-  (let ((text (if (use-region-p)
-                  (buffer-substring start end)
-                (thing-at-point 'word))))
-    (vterm-prep nil)
-    (insert text "\n")))
+  (let ((vterm-prep--new-command-value (if (use-region-p)
+                                           (buffer-substring start end)
+                                         (thing-at-point 'word))))
+    (vterm-prep nil)))
+
+(defun vterm-prep--new-command-entry ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (beginning-of-buffer)
+    (newline)
+    (let ((start (point))
+          (sep-start)
+          (sep-end))
+      (if (boundp 'vterm-prep--new-command-value)
+          (insert vterm-prep--new-command-value))
+      (newline)
+      (let ((o (make-overlay start (point))))
+        (overlay-put o 'face '(:extend t :background "#eee6ce"))
+        (overlay-put o 'field t))
+      (newline)
+      (add-text-properties (- (point) 2) (point) '(read-only t))
+      (goto-char start))))
 
 (define-key vterm-mode-map (kbd "C-x C-e")
   (lambda ()
